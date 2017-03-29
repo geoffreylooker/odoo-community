452f78cbd565dae1bcfd8c8c25c70605d7bb8c81
geoffreylooker <geoffreylooker@users.noreply.github.com>
2017-03-29 16:55:54 +1100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,51 +1,24 @@
-# The Google App Engine base image is debian (jessie) with ca-certificates
-# installed.
-# Source: https://github.com/GoogleCloudPlatform/debian-docker
-#FROM gcr.io/google-appengine/debian8
+#!/bin/bash
 
-#ADD resources /resources
-#ADD scripts /scripts
 
-# Install Python, pip, and C dev libraries necessary to compile the most popular
-# Python libraries.
 ./scripts/install-apt-packages.sh
 
 # Setup locale. This prevents Python 3 IO encoding issues.
-#ENV LANG C.UTF-8
-# Make stdout/stderr unbuffered. This prevents delay between output and cloud
-# logging collection.
-#ENV PYTHONUNBUFFERED 1
+export LANG=en_US.UTF-8
 
 # Upgrade pip (debian package version tends to run a few version behind) and
 # install virtualenv system-wide.
 pip install --upgrade pip virtualenv pysftp
 
-# Install the Google-built interpreters
-#ADD interpreters.tar.gz /
-
-# Add Google-built interpreters to the path
-#ENV PATH /opt/python3.5/bin:$PATH
-
-# Setup the app working directory
-#RUN ln -s /home/vmagent/app /app
-#WORKDIR /app
-
-# Port 8080 is the port used by Google App Engine for serving HTTP traffic.
-#EXPOSE 8080
-#ENV PORT 8080
-
-# The user's Dockerfile must specify an entrypoint with ENTRYPOINT or CMD.
-#CMD []
-
 #########################
 
 # create and activate a virtualenv
-#virtualenv venv
-#. ./venv/bin/activate
+virtualenv venv
+. ./venv/bin/activate
 
 # install Odoo 10.0 nightly
-#pip install -r https://raw.githubusercontent.com/odoo/odoo/10.0/requirements.txt
-#pip install https://nightly.odoo.com/10.0/nightly/src/odoo_10.0.latest.zip
+pip install -r https://raw.githubusercontent.com/odoo/odoo/10.0/requirements.txt
+pip install https://nightly.odoo.com/10.0/nightly/src/odoo_10.0.latest.zip
 
 # check installed packages
 python -c "import odoo" ; || exit $?
@@ -57,6 +30,6 @@ pip list | grep odoo ;
 # start odoo
 #odoo
 
-
+exit 0
 
 
