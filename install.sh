c21ec4ffceef508715aed39b800a1fb0355e33f1
geoffreylooker <geoffreylooker@users.noreply.github.com>
2017-03-30 16:22:42 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-set -e
-
-readonly ODOO_DIR="/opt/odoo-dev"
-
 # Must be root to use this tool
 if [[ ! $EUID -eq 0 ]];then
   if [ -x "$(command -v sudo)" ];then
@@ -15,50 +11,59 @@ if [[ ! $EUID -eq 0 ]];then
   fi
 fi
 
-echo "running install script"
+echo "-----------------------------------------------------------"
+echo "Starting Odoo installation"
+echo "-----------------------------------------------------------"
 
-echo "installing apt packages"
+echo "-----------------------------------------------------------"
+echo "Installing Packages Dependencies"
+echo "-----------------------------------------------------------"
 ./scripts/install-apt-packages.sh
 
 # Setup locale. This prevents Python 3 IO encoding issues.
 export LANG=en_US.UTF-8
 
-# Upgrade pip (debian package version tends to run a few version behind) and
-# install virtualenv system-wide.
-pip install --upgrade pip virtualenv pysftp
+echo "-----------------------------------------------------------"
+echo "Upgrading pip and installing virtualenv system-wide."
+echo "-----------------------------------------------------------"
+pip install --upgrade pip virtualenv
 
-# Less CSS via nodejs 
-# sudo apt-get install -y npm 
+echo "-----------------------------------------------------------"
+echo "Installing Less CSS using"
+echo "-----------------------------------------------------------"
 ln -sf /usr/bin/nodejs /usr/bin/node
-# Once npm is installed, use it to install less:
 npm install -g less
 
-# install Odoo 10.0 nightly
+echo "-----------------------------------------------------------"
+echo "Installing Odoo 10.0 from nightly build"
+echo "-----------------------------------------------------------"
 pip install -r https://raw.githubusercontent.com/odoo/odoo/10.0/requirements.txt
 pip install https://nightly.odoo.com/10.0/nightly/src/odoo_10.0.latest.zip
-
-# check installed packages
-python -c "import odoo" && echo "odoo python module installed";
+# python -c "import odoo"
 #pip list | grep odoo ;
 
-# where should this go
-pip install --upgrade setuptools-odoo
+echo "-----------------------------------------------------------"
+echo "Installing/upgrading setuptools-odoo and pysftp"
+echo "-----------------------------------------------------------"
+pip install --upgrade setuptools-odoo pysftp
 
-echo "installing wkhtmltox"
-#cd /tmp
+echo "-----------------------------------------------------------"
+echo "Installing wkhtmltox"
+echo "-----------------------------------------------------------"
 curl -sO http://download.gna.org/wkhtmltopdf/0.12/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz ;
 tar -xvf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz ;
 cp wkhtmltox/bin/wkhtmltopdf /usr/bin/ ;
 
-# Then tell pip where to find packages that are not on pypi:
+echo "-----------------------------------------------------------"
+echo "Installing Odoo 10.0 OCA addons"
+echo "-----------------------------------------------------------"
 export PIP_FIND_LINKS="https://wheelhouse.odoo-community.org/oca"
-
-echo "installing odoo-oca-server-tools"
 pip install -r https://raw.githubusercontent.com/OCA/server-tools/10.0/requirements.txt
-
 pip install odoo10_addon_base_technical_features
 pip install odoo10_addon_auto_backup
 
-echo "finished $0"
+echo "-----------------------------------------------------------"
+echo "Finished Odoo installation"
+echo "-----------------------------------------------------------"
 
-exit 0
+
