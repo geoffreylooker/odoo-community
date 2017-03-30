c82923ca17740d01c7595b0313a61756bea97fab
geoffreylooker <geoffreylooker@users.noreply.github.com>
2017-03-31 00:12:59 +1100

    Update install-apt-packages.sh

--- scripts/install-apt-packages.sh
+++ scripts/install-apt-packages.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DEBIAN_FRONTEND=noninteractive apt-get update -q
+apt-get update -q
 
 xargs -a <(awk '/^\s*[^#]/' 'resources/apt-packages.txt') -r -- \
-    DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -yq
+    apt-get install --no-install-recommends -yq
 
 # Remove unneeded files.
-DEBIAN_FRONTEND=noninteractive apt-get clean
+apt-get clean
 # rm /var/lib/apt/lists/*_*
