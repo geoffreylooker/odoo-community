897351ce0f826c9ff776661d6374131076f18a41
geoffreylooker <geoffreylooker@users.noreply.github.com>
2017-04-05 06:46:18 +1000

    Create 95_https_nginx_conf.sh

---
+++ scripts/95_https_nginx_conf.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+echo "-----------------------------------------------------------"
+echo "Start - Configuring ssl certificates & nginx"
+echo "-----------------------------------------------------------"
+mkdir -p /etc/nginx/ssl
+cp odoo_geoffreylooker_com.* /etc/nginx/ssl/ 
+chown -R root:root /etc/nginx/ssl
+chmod -R 600 /etc/nginx/ssl
+# root:www 640?
+
+echo "-----------------------------------------------------------"
+echo "Configuring Nginx"
+echo "-----------------------------------------------------------"
+cp resources/odoo-nginx.conf /etc/nginx/sites-available/odoo
+ln -sf /etc/nginx/sites-available/odoo /etc/nginx/sites-enabled/odoo
+chown root:root /etc/nginx/sites-available/odoo
+chmod 640 /etc/nginx/sites-available/odoo
+# root:www 640?
+systemctl enable nginx
+service nginx restart
+
+echo "-----------------------------------------------------------"
+echo "Start - Configuring ssl certificates & nginx"
+echo "-----------------------------------------------------------"
+
