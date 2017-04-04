469b87c390241ef6f74e067ee96dabcf117cf260
geoffreylooker <geoffreylooker@users.noreply.github.com>
2017-04-05 05:22:26 +1000

    Update 30_xidel-install.sh

--- scripts/30_xidel-install.sh
+++ scripts/30_xidel-install.sh
@@ -13,7 +13,7 @@ else
   scratch=$(mktemp -d -t tmp.XXXXXXXXXX)
   # cd $scratch
   pushd $scratch
-  sudo wget https://sourceforge.net/projects/videlibri/files/Xidel/Xidel%200.9.6/xidel_0.9.6-1_amd64.deb
+  sudo wget -nv https://sourceforge.net/projects/videlibri/files/Xidel/Xidel%200.9.6/xidel_0.9.6-1_amd64.deb
   sudo gdebi --n `basename https://sourceforge.net/projects/videlibri/files/Xidel/Xidel%200.9.6/xidel_0.9.6-1_amd64.deb`
   popd
 fi
