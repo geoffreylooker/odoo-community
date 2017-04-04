8505528e7e5c03ab94320d7c916da35a3d517381
geoffreylooker <geoffreylooker@users.noreply.github.com>
2017-04-05 04:27:11 +1000

    Update 50_pip-packages.sh

--- scripts/50_pip-packages.sh
+++ scripts/50_pip-packages.sh
@@ -5,12 +5,21 @@ set -e
 echo "-----------------------------------------------------------"
 echo "Start - Installing Pip-Packages Dependencies"
 echo "-----------------------------------------------------------"
-
 sudo pip install -U pip
+
+echo "-----------------------------------------------------------"
+echo "Installing virtualenv, setuptools & setuptools-odoo"
+echo "-----------------------------------------------------------"
 sudo pip install -U virtualenv setuptools setuptools-odoo 
 
+echo "-----------------------------------------------------------"
+echo "Installing dependencies/pip-packages.txt"
+echo "-----------------------------------------------------------"
 sudo pip install -U -r dependencies/pip-packages.txt
 
+echo "-----------------------------------------------------------"
+echo "Installing dependencies/pip-url-requirement-files.txt"
+echo "-----------------------------------------------------------"
 xargs -a <(awk '/^\s*[^#]/' 'dependencies/pip-url-requirement-files.txt') -r -- \
     sudo pip install -U -r 
 
