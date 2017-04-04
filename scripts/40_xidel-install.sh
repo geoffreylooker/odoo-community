138f51a69b2117e89fe352c1bb48eda679fc5f5b
geoffreylooker <geoffreylooker@users.noreply.github.com>
2017-04-05 03:52:07 +1000

    Update 40_xidel-install.sh

--- scripts/40_xidel-install.sh
+++ scripts/40_xidel-install.sh
@@ -6,11 +6,17 @@ echo "-----------------------------------------------------------"
 echo "Installing xidel_0.9.6-1_amd64"
 echo "-----------------------------------------------------------"
 
-scratch=$(mktemp -d -t tmp.XXXXXXXXXX)
-cd $scratch
-sudo wget https://sourceforge.net/projects/videlibri/files/Xidel/Xidel%200.9.6/xidel_0.9.6-1_amd64.deb
-sudo gdebi --n `basename https://sourceforge.net/projects/videlibri/files/Xidel/Xidel%200.9.6/xidel_0.9.6-1_amd64.deb`
+if command -v xidel >/dev/null 2>&1
+then 
+  echo "xidel is already installed"
+else
+  scratch=$(mktemp -d -t tmp.XXXXXXXXXX)
+  cd $scratch
+  sudo wget https://sourceforge.net/projects/videlibri/files/Xidel/Xidel%200.9.6/xidel_0.9.6-1_amd64.deb
+  sudo gdebi --n `basename https://sourceforge.net/projects/videlibri/files/Xidel/Xidel%200.9.6/xidel_0.9.6-1_amd64.deb`
+fi
 
 echo "-----------------------------------------------------------"
 echo "Finished - Installing xidel_0.9.6-1_amd64"
 echo "-----------------------------------------------------------"
+
