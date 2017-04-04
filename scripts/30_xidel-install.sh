4a0a5b7cda5ac6253dddb08acf1873fd4fcc1242
geoffreylooker <geoffreylooker@users.noreply.github.com>
2017-04-05 05:14:22 +1000

    Update 30_xidel-install.sh

--- scripts/30_xidel-install.sh
+++ scripts/30_xidel-install.sh
@@ -11,9 +11,11 @@ then
   echo "xidel is already installed"
 else
   scratch=$(mktemp -d -t tmp.XXXXXXXXXX)
-  cd $scratch
+  # cd $scratch
+  pushd $scratch
   sudo wget https://sourceforge.net/projects/videlibri/files/Xidel/Xidel%200.9.6/xidel_0.9.6-1_amd64.deb
   sudo gdebi --n `basename https://sourceforge.net/projects/videlibri/files/Xidel/Xidel%200.9.6/xidel_0.9.6-1_amd64.deb`
+  popd
 fi
 
 echo "-----------------------------------------------------------"
