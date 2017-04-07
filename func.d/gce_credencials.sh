6a34de44994f5d17af6d1ea87552bd5cef214cc2
geoffreylooker <geoffreylooker@users.noreply.github.com>
2017-04-07 19:52:20 +1000

    Create gce_credencials.sh

---
+++ func.d/gce_credencials.sh
@@ -0,0 +1,9 @@
+# TODO: create and configure service account/creditials file
+# 
+# https://cloud.google.com/sdk/gcloud/reference/beta/iam/service-accounts/create
+# gcloud beta iam service-accounts create
+# gcloud beta iam service-accounts create NAME [--display-name=DISPLAY_NAME] [GLOBAL-FLAG …]
+#
+gcloud beta iam service-accounts create odoo-gce --display-name "Odoo Server GCE"
+#> Created service account [odoo-gce].
+#
